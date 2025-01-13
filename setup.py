99bbee4bc4717f04ca7e915350d5d533a374dab7
somethingreallycool123 <113038510+somethingreallycool123@users.noreply.github.com>
2025-01-14 03:25:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="code_assist",  
+    name="codemate_ai",  
     version="0.1.0",    
     packages=find_packages(),  
     install_requires=[  
