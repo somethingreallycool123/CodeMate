a912b4cdb2fc35d8e8dd949b80903606fa2f0f13
somethingreallycool123 <113038510+somethingreallycool123@users.noreply.github.com>
2025-01-14 00:14:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="code_assist",  # The name of your package
-    version="0.1.0",    # The version of your package
-    packages=find_packages(),  # Automatically find all the subpackages in the code_assist directory
-    install_requires=[  # List of dependencies that need to be installed
+    name="code_assist",  
+    version="0.1.0",    
+    packages=find_packages(),  
+    install_requires=[  
         "torch",
         "transformers",
         "huggingface_hub",
@@ -14,16 +14,19 @@ setup(
         "nbformat",
         "pytest",
         "pytorch-lightning",
+        "jupyter_server",
+        "astor",
+        "pygments"
     ],
-    entry_points={  # If you want to create a command-line tool
+    entry_points={  l
         "console_scripts": [
-            "code-assist=code_assist.main:main",  # Modify based on your project's entry point
+            "code-assist=code_assist.main:main",  
         ],
     },
-    classifiers=[  # Optional: help categorize your project
+    classifiers=[  
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Optional: specify Python version requirement
+    python_requires='>=3.6',  
 )
