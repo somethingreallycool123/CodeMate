6e54c57bb34f4fc630e4d11ac5f06695b1e6390e
somethingreallycool123 <113038510+somethingreallycool123@users.noreply.github.com>
2025-01-18 17:26:16 +0530

    Update __init__.py

--- codemate_ai/__init__.py
+++ codemate_ai/__init__.py
@@ -4,3 +4,6 @@ from .providers import config, LLMProvider
 def load_ipython_extension(ipython):
     """Load the extension in IPython."""
     ipython.register_magics(CodeAssistMagics)
+
+def unload_ipython_extension(ipython):
+    pass
